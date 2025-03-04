715673941f93df44ce6ec5a8e08e5ca36bb7b2d8
dmike16 <cipmiky@gmail.com>
2025-03-04 23:50:05 +0100

    feat: remove not used config

--- lua/theprimeagen/lazy/init.lua
+++ lua/theprimeagen/lazy/init.lua
@@ -3,9 +3,6 @@ return {
         "vhyrro/luarocks.nvim",
         config = true,
         priority = 1000,
-        opts = {
-            rocks = { "magick" },
-        },
     },
     {
         "nvim-lua/plenary.nvim",
