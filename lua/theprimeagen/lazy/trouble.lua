9409c88bbd8a7b9c0745ba81d7a588375bf296c4
dmike16 <cipmiky@gmail.com>
2025-03-04 22:28:50 +0100

    fix: correct trouble config

--- lua/theprimeagen/lazy/trouble.lua
+++ lua/theprimeagen/lazy/trouble.lua
@@ -7,7 +7,7 @@ return {
             })
 
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
+                require("trouble").toggle("diagnostics")
             end)
 
             vim.keymap.set("n", "[t", function()
@@ -15,7 +15,7 @@ return {
             end)
 
             vim.keymap.set("n", "]t", function()
-                require("trouble").previous({skip_groups = true, jump = true});
+                require("trouble").prev({skip_groups = true, jump = true});
             end)
 
         end
