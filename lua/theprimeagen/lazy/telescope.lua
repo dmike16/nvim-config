5ca4908962db538265aea35c4dae6fb349fd73c1
dmike16 <cipmiky@gmail.com>
2025-03-22 17:48:05 +0100

    feat: add telescopre cmdline extensions

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -4,11 +4,13 @@ return {
     tag = "0.1.8",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        'jonarrien/telescope-cmdline.nvim',
     },
 
     config = function()
         require('telescope').setup({})
+        require('telescope').load_extension("cmdline")
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc='Telescope find files'})
@@ -28,6 +30,7 @@ return {
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {desc='Telescope help tags'})
         vim.keymap.set('n', '<leader>pb', builtin.buffers, {desc='Telescope buffers'})
+        vim.keymap.set('n', '<leader>q',':Telescope cmdline<CR>', { noremap = true, desc = "Cmdline" })
     end
 }
 
